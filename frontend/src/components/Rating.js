94512eb0341ace66fd466f9980bbed69bb728d4b
RichardVerbraak <RichardVerbraak@users.noreply.github.com>
2020-10-05 16:23:25 +0200

    Rating component complete

--- frontend/src/components/Rating.js
+++ frontend/src/components/Rating.js
@@ -1,5 +1,6 @@
 import React from 'react'
 
+// Checks against rating value and adds a star, half-star or empty star if condition is met
 const Rating = ({ value, text }) => {
 	return (
 		<div className='rating'>
@@ -10,10 +11,55 @@ const Rating = ({ value, text }) => {
 							? 'fas fa-star'
 							: value >= 0.5
 							? 'fas fa-star-half-alt'
-							: 'fas fa-star'
+							: 'far fa-star'
 					}
 				></i>
 			</span>
+			<span>
+				<i
+					className={
+						value >= 2
+							? 'fas fa-star'
+							: value >= 1.5
+							? 'fas fa-star-half-alt'
+							: 'far fa-star'
+					}
+				></i>
+			</span>
+			<span>
+				<i
+					className={
+						value >= 3
+							? 'fas fa-star'
+							: value >= 2.5
+							? 'fas fa-star-half-alt'
+							: 'far fa-star'
+					}
+				></i>
+			</span>
+			<span>
+				<i
+					className={
+						value >= 4
+							? 'fas fa-star'
+							: value >= 3.5
+							? 'fas fa-star-half-alt'
+							: 'far fa-star'
+					}
+				></i>
+			</span>
+			<span>
+				<i
+					className={
+						value >= 5
+							? 'fas fa-star'
+							: value >= 4.5
+							? 'fas fa-star-half-alt'
+							: 'far fa-star'
+					}
+				></i>
+			</span>
+			<span>{text && text}</span>
 		</div>
 	)
 }
