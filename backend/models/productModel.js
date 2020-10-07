98df1f9c398506b253757ebe108a945ae9ea6ab2
RichardVerbraak <RichardVerbraak@users.noreply.github.com>
2020-10-07 16:37:34 +0200

    Created Product Model

---
+++ backend/models/productModel.js
@@ -0,0 +1,66 @@
+import mongoose from 'mongoose'
+
+// Reviews will be an array that consists of this object (review)
+const reviewSchema = new mongoose.Schema(
+	{
+		name: { type: String, required: true },
+		rating: { type: Number, required: true },
+		comment: { type: String, required: true },
+	},
+	{ timestamps: true }
+)
+
+const productSchema = new mongoose.Schema(
+	{
+		// Ref creates the relationship between product and user
+		user: {
+			type: mongoose.Schema.Types.ObjectId,
+			required: true,
+			ref: 'User',
+		},
+		image: {
+			type: String,
+			required: true,
+		},
+		brand: {
+			type: String,
+			required: true,
+		},
+		category: {
+			type: String,
+			required: true,
+		},
+		description: {
+			type: String,
+			required: true,
+		},
+		reviews: [reviewSchema],
+		rating: {
+			type: Number,
+			required: true,
+			default: 0,
+		},
+		numReviews: {
+			type: Number,
+			required: true,
+			default: 0,
+		},
+		price: {
+			type: Number,
+			required: true,
+			default: 0,
+		},
+		countInStock: {
+			type: Number,
+			required: true,
+			default: 0,
+		},
+	},
+	{
+		timestamps: true,
+	}
+)
+
+const Product = mongoose.model('Product', productSchema)
+
+export default Product
