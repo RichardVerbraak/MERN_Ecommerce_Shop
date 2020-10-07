e126ca878017d3f5c1ff9d82f6ecd7ec24c4c218
RichardVerbraak <RichardVerbraak@users.noreply.github.com>
2020-10-07 16:32:07 +0200

    Created User Model

---
+++ backend/models/userModel.js
@@ -0,0 +1,32 @@
+import mongoose from 'mongoose'
+
+// Created at and updated add could be manually added but also with timestamps set to true
+const userSchema = new mongoose.Schema(
+	{
+		name: {
+			type: String,
+			required: true,
+		},
+		email: {
+			type: String,
+			required: true,
+			unique,
+		},
+		password: {
+			type: String,
+			required: true,
+		},
+		isAdmin: {
+			type: Boolean,
+			required: true,
+			default: false,
+		},
+	},
+	{
+		timestamps: true,
+	}
+)
+
+const User = mongoose.model('user', userSchema)
+
+export default User
