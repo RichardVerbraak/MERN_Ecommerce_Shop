e4a3bce9960687147a27fc204ead396e676977f3
RichardVerbraak <RichardVerbraak@users.noreply.github.com>
2020-10-12 18:05:02 +0200

    Added product list reducer

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -5,9 +5,9 @@ import Rating from './Rating'
 
 const Product = ({ product }) => {
 	return (
-		<Card className='my-3 py-3 rounded'>
+		<Card className='my-3 p-3 rounded'>
 			<Link to={`/product/${product._id}`}>
-				<Card.Img src={product.image} variant='top'></Card.Img>
+				<Card.Img src={product.image} variant='top' />
 			</Link>
 
 			<Card.Body>
@@ -16,12 +16,14 @@ const Product = ({ product }) => {
 						<strong>{product.name}</strong>
 					</Card.Title>
 				</Link>
+
 				<Card.Text as='div'>
 					<Rating
 						value={product.rating}
 						text={`${product.numReviews} reviews`}
 					/>
 				</Card.Text>
+
 				<Card.Text as='h3'>${product.price}</Card.Text>
 			</Card.Body>
 		</Card>
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -2,7 +2,9 @@ import { createStore, combineReducers, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
 import { composeWithDevTools } from 'redux-devtools-extension'
 
-const reducer = combineReducers({})
+import { productListReducer } from './reducers/productReducers'
+
+const reducer = combineReducers({ productListReducer })
 
 const initialState = {}
 
