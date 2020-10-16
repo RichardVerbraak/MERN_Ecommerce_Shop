fa4a4c9eb2f911442de16eb76f55a87053e85497
RichardVerbraak <RichardVerbraak@users.noreply.github.com>
2020-10-16 16:57:52 +0200

    Additional comments about the process of saving to Redux and then localStorage

--- frontend/src/actions/cartActions.js
+++ frontend/src/actions/cartActions.js
@@ -6,7 +6,9 @@ export const addCartItem = (id, qty) => {
 			// Already get json back from promise due to axios, no need for res.json()
 			const { data } = await axios.get(`/api/products/${id}`)
 
+			// Save to Redux
 			// Quantity is already coming in from cartScreen
+			// Maybe use {...payload.action, qty} ?
 			dispatch({
 				type: 'ADD_CART_ITEM',
 				payload: {
@@ -19,6 +21,7 @@ export const addCartItem = (id, qty) => {
 				},
 			})
 
+			// Save to localStorage after it's in the Redux state with getState
 			localStorage.setItem(
 				'cartItems',
 				JSON.stringify(getState().cart.cartItems)
