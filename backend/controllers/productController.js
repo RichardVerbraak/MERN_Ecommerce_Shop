0db9b15ff7ad92253d6d6c5afa3baa2d6da6a8ad
RichardVerbraak <RichardVerbraak@users.noreply.github.com>
2020-10-20 15:31:36 +0200

    Moved logic in the routes to a controller folder for clean up

---
+++ backend/controllers/productController.js
@@ -0,0 +1,31 @@
+import asyncHandler from 'express-async-handler'
+import Product from '../models/productModel.js'
+
+// Instead of having a try/catch in every route we use asyncHandler
+// It passes exceptions inside async routes (things outside of the normal expectations aka errors) to your express error handlers
+
+// @desc        Gets all products
+// @route       GET /api/products
+// @access      Public
+const getProducts = asyncHandler(async (req, res) => {
+	// Gets all products (Empty object just gets everything)
+	const products = await Product.find({})
+
+	res.json(products)
+})
+
+// @desc        Gets single product by ID
+// @route       GET /api/products/:id
+// @access      Public
+const getProductbyID = asyncHandler(async (req, res) => {
+	const product = await Product.findById(req.params.id)
+
+	if (product) {
+		res.json(product)
+	} else {
+		res.status(400)
+		throw new Error('Product not found')
+	}
+})
+
+export { getProducts, getProductbyID }
--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,42 +1,15 @@
 import express from 'express'
-import asyncHandler from 'express-async-handler'
-import Product from '../models/productModel.js'
-
+import {
+	getProducts,
+	getProductbyID,
+} from '../controllers/productController.js'
 const router = express.Router()
 
-// Routes are the sames as controllers, they perform logic based on a certain request like the below routes
+// Controllers perform logic based on a certain request, we moved these out of the folder just to clean it up
 
-// Instead of having a try/catch in every route we use asyncHandler
-// It passes exceptions inside async routes (things outside of the normal expectations aka errors) to your express error handlers
+// They both do the same, syntax is just slightly different
+router.route('/').get(getProducts)
 
-// @desc        Gets all products
-// @route       GET /api/products
-// @access      Public
-router.get(
-	'/',
-	asyncHandler(async (req, res) => {
-		// Gets all products (Empty object just gets everything)
-		const products = await Product.find({})
-
-		res.json(products)
-	})
-)
-
-// @desc        Gets single product by ID
-// @route       GET /api/products/:id
-// @access      Public
-router.get(
-	'/:id',
-	asyncHandler(async (req, res) => {
-		const product = await Product.findById(req.params.id)
-
-		if (product) {
-			res.json(product)
-		} else {
-			res.status(404)
-			throw new Error('No product found with that ID')
-		}
-	})
-)
+router.get('/:id', getProductbyID)
 
 export default router
