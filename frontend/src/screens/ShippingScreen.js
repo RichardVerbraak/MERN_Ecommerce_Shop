5180b2086fa89d664ca697c0212a8f284449f7f3
RichardVerbraak <RichardVerbraak@users.noreply.github.com>
2020-11-02 16:47:14 +0100

    Added Checkout Steps to the shipping screen. If you are on the shipping page you've already logged in so step1 (sign in) and step2 (shipping) will be passed to the component

--- frontend/src/screens/ShippingScreen.js
+++ frontend/src/screens/ShippingScreen.js
@@ -3,6 +3,7 @@ import { Form, Button } from 'react-bootstrap'
 import { useDispatch, useSelector } from 'react-redux'
 import { saveShippingAddress } from '../actions/cartActions'
 import FormContainer from '../components/FormContainer'
+import CheckoutSteps from '../components/CheckoutSteps'
 
 // Could also use useEffect for setting address?
 const ShippingScreen = ({ history }) => {
@@ -33,6 +34,7 @@ const ShippingScreen = ({ history }) => {
 
 	return (
 		<FormContainer>
+			<CheckoutSteps step1 step2 />
 			<h1>Shipping</h1>
 			<Form onSubmit={submitHandler}>
 				<Form.Group controlId='address'>
