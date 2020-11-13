0a4d0661ca311d8f86eeade5aad8043112b7fb34
RichardVerbraak <RichardVerbraak@users.noreply.github.com>
2020-11-13 17:09:22 +0100

    CreateProductReducer added

--- frontend/src/reducers/productReducers.js
+++ frontend/src/reducers/productReducers.js
@@ -59,6 +59,27 @@ export const productDetailReducer = (
 	}
 }
 
+export const productCreateReducer = (state = {}, action) => {
+	switch (action.type) {
+		case 'PRODUCT_CREATE_REQUEST':
+			return {
+				loading: true,
+			}
+		case 'PRODUCT_CREATE_SUCCESS':
+			return {
+				loading: false,
+				success: true,
+			}
+		case 'PRODUCT_CREATE_FAIL':
+			return {
+				loading: false,
+				error: action.payload,
+			}
+		default:
+			return state
+	}
+}
+
 export const productDeleteReducer = (state = {}, action) => {
 	switch (action.type) {
 		case 'PRODUCT_DELETE_REQUEST':
